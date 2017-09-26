0047c018632ec127879e9c95187764c70111ed13
Galen Koon <galen.95@live.com>
2017-09-26 11:51:36 -0600

    HW 3 Part 1

---
+++ HW 3/sketch.js
@@ -0,0 +1,14 @@
+// Declare a setup function
+function setup(){
+  //creating a canvas 500px by 500px
+  createCanvas( 500, 500);
+  //coloring the background 'orange'
+  background( 'orange');
+}
+
+//Declare a draw function
+function draw(){
+  //Tell p5 to print a text labeled "Hello World!"
+  //This will be positioned at (x:50, y:100)
+  text( "Hello World!", 50, 100);
+}
