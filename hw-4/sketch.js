0a56af741bc7e4e627554db7487946a6d3c0e752
Galen Koon <galen.95@live.com>
2017-10-04 13:18:17 -0600

    setup hw 4 character

---
+++ hw-4/sketch.js
@@ -0,0 +1,31 @@
+//setup function
+function setup() {
+  //create canvas to draw on
+  createCanvas (500, 500);
+}
+function draw() {
+  //background color
+  background( rgb(165, 250, 186));
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
