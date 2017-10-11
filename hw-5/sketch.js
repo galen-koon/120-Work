139bef0062da248391f8dd8b3c8357a64242c614
Galen Koon <galen.95@live.com>
2017-10-11 01:13:12 -0600

    housekeeping

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -20,11 +20,14 @@ function draw(){
   noCursor();
 //specifying that I do not want a cursor. Instead, I will have a car
   push();
+//body of car
   fill('red');
   rect( mouseX, mouseY, 70, 50, 10, 90, 20, 0);
+//wheels  
   fill('black');
   ellipse( mouseX + 3, mouseY + 50, 30);
   ellipse( mouseX + 70, mouseY + 50, 30);
+
   pop();
 
 //creating my stop light. This will be composed of a rectangle and 3 circles.
@@ -33,24 +36,31 @@ function draw(){
   push();
   fill('black');
   rect( width * .5 - 50, height * .25, 100, 300);
+
   pop();
 
 //red light at the top
   push();
+
   fill('red');
   ellipse( width * .5, 220, 60);
+
   pop();
 
 //yellow light in middle
   push();
+
   fill('yellow');
   ellipse(width * .5, 300, 60);
+
   pop();
 
 //green light at the bottom
   push();
+
   fill('green');
   ellipse(width * .5, 380, 60);
+
   pop();
 
   pop();
